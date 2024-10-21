e85ee0e2d6ade6ddec60c1e5e84dfc1b880d7a3b
Ananyas24 <143926350+Ananyas24@users.noreply.github.com>
2024-10-21 11:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || '*', // Use the deployed frontend URL for production
+    origin: process.env.FRONTEND_URL || 'https://job-portal-frontend-5xls.onrender.com', // Use the deployed frontend URL for production
     credentials: true,
 };
 app.use(cors(corsOptions));
